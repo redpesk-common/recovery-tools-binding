4d34683c77af399739a7e7e1e47c5770e8dfccab
Sylvain MOCARD <sylvain.mocard@iot.bzh>
2024-09-02 14:28:49 +0200

    fixed file hierarchy

--- set-git-tag.js
+++ set-git-tag.js
@@ -8,17 +8,24 @@ const gitTag = execSync('git describe --tags --abbrev=0').toString().trim();
 // Define the environment file path
 const envFilePathProd = path.join(__dirname, 'src', 'environments', 'environment.prod.ts');
 const envFilePath = path.join(__dirname, 'src', 'environments', 'environment.ts');
+const pkgFilePath = path.join(__dirname, 'package.json');
 
 // Read the existing environment file
 let envFileContentProd = fs.readFileSync(envFilePathProd, 'utf8');
 let envFileContent = fs.readFileSync(envFilePath, 'utf8');
+let pkgFileContent = fs.readFileSync(pkgFilePath, 'utf8');
 
 // Inject the git tag into the environment file
 envFileContentProd = envFileContentProd.replace(/GIT_TAG: '.*'/, `GIT_TAG: '${gitTag}'`);
 envFileContent = envFileContent.replace(/GIT_TAG: '.*'/, `GIT_TAG: '${gitTag}'`);
 
+// Update the version in package.json
+pkgFileContent = pkgFileContent.replace(/"version": ".*"/, `"version": "${gitTag}"`);
+
 // Write the updated environment file back to disk
 fs.writeFileSync(envFilePath, envFileContent, 'utf8');
 fs.writeFileSync(envFilePathProd, envFileContentProd, 'utf8');
+fs.writeFileSync(pkgFilePath, pkgFileContent, 'utf8');
 
 console.log(`Set GIT_TAG to ${gitTag} in environment.prod.ts`);
+console.log(`Set version to ${gitTag} in package.json`);
